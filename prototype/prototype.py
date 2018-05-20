f43e5b13f209ae6ad05ec69d79fee6456413834a
Brendon J. Brewer <brendon.brewer@gmail.com>
2018-05-20 17:19:11 +1200

    UCC map

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -14,27 +14,63 @@ def generate(n):
              "us": rng.rand(n) }
 
 @jit
-def compute_uccs(particles):
+def compute_particle_uccs(particles):
     """
     Compute the non-integer UCCs.
     """
-    uccs = np.empty(len(particles["xs"]))
+    uccs = np.empty(len(particles["xs"]), dtype="float64")
     for i in range(len(particles["xs"])):
-        above = (particles["xs"] > particles["xs"][i]) & \
-                (particles["ys"] > particles["ys"][i])
-        uccs[i] = np.sum(above) + particles["us"][i]
+        uccs[i] = compute_ucc(particles["xs"][i],
+                              particles["ys"][i],
+                              particles)
+    uccs += particles["us"]
     return uccs
 
-if __name__ == "__main__":
-    num_particles = 100
-    rng.seed(0)
-    particles = generate(num_particles)
-    uccs = compute_uccs(particles)
 
-    # Plot initial particles
+
+def compute_ucc(x, y, particles):
+    """
+    Compute the integer UCC at the given position
+    """
+    above = (particles["xs"] > x) & \
+            (particles["ys"] > y)
+    return np.sum(above)
+
+@jit
+def ucc_grid(particles):
+    """
+    Evaluate integer UCC on a grid
+    """
+    # The grid
+    xx = np.linspace(0.0, 1.0, 1001)
+    yy = np.linspace(0.0, 1.0, 1001)
+    [xx, yy] = np.meshgrid(xx, yy[::-1])
+
+    # Compute uccs
+    uccs = np.zeros(xx.shape, dtype="int64")
+    for i in range(xx.shape[0]):
+        for j in range(xx.shape[1]):
+            uccs[i, j] = compute_ucc(xx[i, j], yy[i, j], particles)
+
+    # Pack into a dictionary
+    return { "xx": xx, "yy": yy, "uccs": uccs}
+
+if __name__ == "__main__":
+    num_particles = 10
+    rng.seed(0)
+
+    # Particles and their real-valued UCCs
+    particles = generate(num_particles)
+    uccs = compute_particle_uccs(particles)
+
+    # Integer UCCs on a grid
+    grid = ucc_grid(particles)
+
+    # Plot initial particles and grid
     plt.figure(figsize=(7, 6))
-    plt.scatter(particles["xs"], particles["ys"], s=5.0*uccs,
-                marker="o", alpha=0.3)
+    plt.imshow(grid["uccs"], extent=[0.0, 1.0, 0.0, 1.0])
+    plt.scatter(particles["xs"], particles["ys"],
+                s=10.0*uccs, marker="o", color="w", alpha=0.5)
     plt.axis([0, 1, 0, 1])
     plt.show()
 
