3a793e3ed6869ac87ad361e74c72952cd3fc2d38
Brendon J. Brewer <brendon.brewer@gmail.com>
2018-05-23 19:47:35 +1200

    New prototype

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -1,79 +1,127 @@
 """
 A sandbox for testing ideas
 """
+import dnest4
 import matplotlib.pyplot as plt
 from numba import jit
 import numpy as np
 import numpy.random as rng
 
-def generate(n):
+class Sampler:
     """
-    Generate some initial particles.
+    The current state of a sampler.
     """
-    return { "xs": rng.rand(n), "ys": rng.rand(n),
-             "us": rng.rand(n) }
 
-@jit
-def compute_particle_uccs(particles):
-    """
-    Compute the non-integer UCCs.
-    """
-    uccs = np.empty(len(particles["xs"]), dtype="float64")
-    for i in range(len(particles["xs"])):
-        uccs[i] = compute_ucc(particles["xs"][i],
-                              particles["ys"][i],
-                              particles)
-    uccs += particles["us"]
-    return uccs
+    def __init__(self, num_particles=1000):
+        self.num_particles = num_particles
+
+    def from_prior(self):
+        self.particles = rng.rand(self.num_particles, 2)
+        self.tbs = rng.rand(self.num_particles)
+
+        self.shadow_particles = rng.rand(self.num_particles, 2)
+        self.shadow_tbs = rng.rand(self.num_particles)
+        self.calculate_uccs()
 
 
+    def calculate_ucc(self, x, y):
+        """
+        Calculate the UCC at the given position.
+        """
+        ucc = 0
+        for i in range(self.shadow_particles.shape[0]):
+            if (x < self.shadow_particles[i, 0]) &\
+               (y < self.shadow_particles[i, 1]):
+                ucc += 1
+        return ucc
 
-def compute_ucc(x, y, particles):
-    """
-    Compute the integer UCC at the given position
-    """
-    above = (particles["xs"] > x) & \
-            (particles["ys"] > y)
-    return np.sum(above)
+    def calculate_uccs(self):
+        self.uccs = np.empty(self.num_particles)
+        for i in range(self.num_particles):
+            self.uccs[i] = self.calculate_ucc(self.particles[i, 0],
+                                              self.particles[i, 1])
 
-@jit
-def ucc_grid(particles):
-    """
-    Evaluate integer UCC on a grid
-    """
-    # The grid
-    xx = np.linspace(0.0, 1.0, 201)
-    yy = np.linspace(0.0, 1.0, 201)
-    [xx, yy] = np.meshgrid(xx, yy[::-1])
+    def update(self):
 
-    # Compute uccs
-    uccs = np.zeros(xx.shape, dtype="int64")
-    for i in range(xx.shape[0]):
-        for j in range(xx.shape[1]):
-            uccs[i, j] = compute_ucc(xx[i, j], yy[i, j], particles)
+        # Find worst particle
+        uccs_tbs = [(self.uccs[i], self.tbs[i])\
+                            for i in range(self.num_particles)]
+        worst = 0
+        for i in range(self.num_particles):
+            if uccs_tbs[i] > uccs_tbs[worst]:
+                worst = i
 
-    # Pack into a dictionary
-    return { "xx": xx, "yy": yy, "uccs": uccs}
+        # Result of iteration
+        result = self.particles[worst, :]
+
+        # Threshold
+        threshold = uccs_tbs[worst]
+
+        # Replace particle
+        while True:
+            copy = rng.randint(self.num_particles)
+            if copy != worst:
+                break
+
+        # Copy survivor
+        self.particles[worst, :] = self.particles[copy, :]
+        self.tbs[worst] = self.tbs[copy]
+
+        # Do MCMC
+        k = worst # Alias
+
+        self.particles[k, :], self.tbs[k], self.uccs[k] = \
+                self.explore(self.particles[k, :], self.tbs[k], self.uccs[k],
+                                threshold)
+
+        # Check for any bad shadow particles
+        replace = []
+        for i in range(self.shadow_particles.shape[0]):
+            ucc = self.calculate_ucc(self.shadow_particles[i, 0],
+                                     self.shadow_particles[i, 1])
+            pair = (ucc, self.shadow_tbs[i])
+            if pair > threshold:
+                particle = self.shadow_particles[i, :].copy()
+                tb = self.shadow_tbs[i].copy()
+                particle, tb, ucc = self.explore(particle,
+                                                 tb, ucc,
+                                                 threshold)
+                replace.append((i, particle, tb))
+
+        for r in replace:
+            i, particle, tb = r
+            self.shadow_particles[i, :] = particle
+            self.shadow_tbs[i] = tb
+
+        return result
+
+    def explore(self, particle, tb, ucc, threshold):
+
+        for i in range(1000):
+            proposal = particle.copy()
+            proposal[0] = dnest4.wrap(proposal[0] + dnest4.randh(), 0.0, 1.0)
+            proposal[1] = dnest4.wrap(proposal[1] + dnest4.randh(), 0.0, 1.0)
+            proposal_tb = dnest4.wrap(tb + dnest4.randh(), 0.0, 1.0)
+            proposal_ucc = self.calculate_ucc(proposal[0], proposal[1])
+
+            if (proposal_ucc, proposal_tb) < threshold:
+                particle = proposal
+                tb = proposal_tb
+                ucc = proposal_ucc
+
+        return [particle, tb, ucc]
 
 if __name__ == "__main__":
-    num_particles = 2
+    sampler = Sampler(100)
+    sampler.from_prior()
 
-    # Particles and their real-valued UCCs
-    particles = generate(num_particles)
-    uccs = compute_particle_uccs(particles)
-    worst_ucc = uccs.max()
+#    plt.scatter(sampler.shadow_particles[:,0], sampler.shadow_particles[:,1],
+#                marker="o", color="k", alpha=0.2)
+    for i in range(0, 500):
+        result = sampler.update()
+        plt.scatter(result[0], result[1], marker=".", color="r", alpha=0.3)
+        print(i+1)
 
-    # Integer UCCs on a grid
-    grid = ucc_grid(particles)
-#    print("Prior mass remaining:")
-#    print(1.0 - (grid["uccs"] >= worst_ucc).sum()*0.005**2)
-
-    # Plot initial particles and grid
-    plt.figure(figsize=(7, 6))
-    plt.imshow(grid["uccs"], extent=[0.0, 1.0, 0.0, 1.0],
-               cmap="Oranges")
-    plt.scatter(particles["xs"], particles["ys"],
-                s=20.0*uccs, marker="o", color="k", alpha=0.5)
-    plt.axis([0, 1, 0, 1])
     plt.show()
 
+    
