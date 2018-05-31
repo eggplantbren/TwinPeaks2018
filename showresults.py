49762bf7fa5cd08af3ffbf8f2b9f051f9ae11cdd
Brendon J. Brewer <brendon.brewer@gmail.com>
2018-06-01 10:51:00 +1200

    Helpful comments

--- showresults.py
+++ showresults.py
@@ -1,3 +1,10 @@
 import twinpeaks2018
 
-twinpeaks2018.postprocess_two_scalars()
+# demo=True makes extra plots based on the ground truth of the demo model.
+# If you leave it in while using another model, you'll get some additional
+# output that will be spurious. You can just ignore it, though.
+twinpeaks2018.postprocess_two_scalars(demo=True)
+
+# One-scalar version
+# twinpeaks2018.postprocess_one_scalar()
+
