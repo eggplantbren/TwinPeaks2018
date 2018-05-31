29b615689bd3b3c7568b5652a403d17e356724bb
Brendon J. Brewer <brendon.brewer@gmail.com>
2018-06-01 11:45:28 +1200

    Infer the correct number of scalars

--- showresults.py
+++ showresults.py
@@ -1,10 +1,21 @@
 import twinpeaks2018
 
-# demo=True makes extra plots based on the ground truth of the demo model.
-# If you leave it in while using another model, you'll get some additional
-# output that will be spurious. You can just ignore it, though.
-twinpeaks2018.postprocess_two_scalars(demo=True)
+# Infer whether we're dealing with one or two scalars!
+f = open("output/particles_info.csv")
+columns = (f.readline()).split(",")
+f.close()
 
-# One-scalar version
-# twinpeaks2018.postprocess_one_scalar()
+if len(columns) == 5:
+    # demo=True makes extra plots based on the ground truth of the demo model.
+    # If you leave it in while using another model, you'll get some additional
+    # output that will be spurious. You can just ignore it, though.
+    twinpeaks2018.postprocess_two_scalars(demo=True)
 
+elif len(columns) == 4:
+    # One-scalar version
+    twinpeaks2018.postprocess_one_scalar()
+
+else:
+    print("Unexpected number of columns in output/particles_info.csv.")
+    print("You may be running a model with 3 or more scalars, in which case "\
+                + "you're on your own.")
