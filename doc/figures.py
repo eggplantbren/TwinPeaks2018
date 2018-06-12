e72ccc4e342fe682349c5f1d8ebaf0fce18f88c3
Brendon J. Brewer <brendon.brewer@gmail.com>
2018-06-13 11:56:50 +1200

    New figures

--- doc/figures.py
+++ doc/figures.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import twinpeaks2018 as tp
 
+# Set up the fonts
+tp.plot_settings()
+
 # Compute the true partition function
 limits = [1E-1, 1E2, 1E-1, 1E2]
 truth = tp.demo_truth.compute_truth(limits=limits, n=51)
@@ -22,5 +25,24 @@ plt.title("$H$")
 plt.savefig("figures/demo_truth.pdf")
 print("Saved figures/demo_truth.pdf")
 
-#plt.figure(2)
-#x = np.linspace(0.0, 1.0, 
+x = np.linspace(0.0, 1.0, 1001)
+def s(x, T1, T2):
+    s = -((x - 0.5)/0.1)**2/T1 - np.sin(10.0*np.pi*x)**2/T2
+    return np.exp(s)
+
+plt.figure(2, figsize=(7, 6))
+S = s(x, 10.0, 10.0)
+S = S / np.trapz(S, x=x)
+plt.plot(x, S, label="$(T_1, T_2) = (1, 1)$")
+S = s(x, 10.0, 1.0)
+S = S / np.trapz(S, x=x)
+plt.plot(x, S, label="$(T_1, T_2) = (10, 1)$")
+S = s(x, 1.0, 10.0)
+S = S / np.trapz(S, x=x)
+plt.plot(x, S, label="$(T_1, T_2) = (1, 10)$")
+plt.xlabel("$x$", fontsize=20)
+plt.ylabel("$p(x; T_1, T_2)$", fontsize=20)
+plt.legend()
+plt.savefig("figures/demo_canonical.pdf")
+print("Saved figures/demo_canonical.pdf")
+
