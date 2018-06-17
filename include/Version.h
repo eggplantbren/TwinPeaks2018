e58fa89c5bba20afda877c5fac9006d305ac5bb1
Brendon J. Brewer <brendon.brewer@gmail.com>
2018-06-18 11:23:51 +1200

    Version file

---
+++ include/Version.h
@@ -0,0 +1,9 @@
+#ifndef TwinPeaks2018_Version_h
+#define TwinPeaks2018_Version_h
+
+#define TWINPEAKS2018_MAJOR_VERSION 0
+#define TWINPEAKS2018_MINOR_VERSION 2
+#define TWINPEAKS2018_PATCH_VERSION 1
+
+#endif
+
