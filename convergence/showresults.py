7ec6c4cfe2464ec033b77bf82163d7796a7a8846
Brendon J. Brewer <brendon.brewer@gmail.com>
2019-09-20 12:42:07 +1200

    Axis labels

--- convergence/showresults.py
+++ convergence/showresults.py
@@ -5,10 +5,11 @@ f = open("demo.yaml")
 data = yaml.load(f, Loader=yaml.SafeLoader)
 f.close()
 
-
 xs = [run["reps"] for run in data["runs"]]
 ys = [run["mean_abs_error_lnZ"] for run in data["runs"]]
 
 plt.semilogx(xs, ys, "o")
+plt.xlabel("Number of repetitions")
+plt.ylabel("mean_abs_error_lnZ")
 plt.show()
 
